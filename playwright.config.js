99c9713d0e83b0d1d9546285b2420eda52563fe2
Samuel Jurado <samueljurado12@gmail.com>
2025-12-01 15:41:46 +0100

    Modified config

--- playwright.config.js
+++ playwright.config.js
@@ -1,19 +1,19 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
- import dotenv from 'dotenv';
- import path from 'path';
- dotenv.config();
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config();
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -23,25 +23,25 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: `${process.env.BASE_URL}`,
+    baseURL: `https://gorest.co.in`,
 
     extraHTTPHeaders: {
-      'Accept': 'application/json'
+      Accept: "application/json",
     },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     /* Test against mobile viewports. */
@@ -65,4 +65,3 @@ export default defineConfig({
     // },
   ],
 });
-
